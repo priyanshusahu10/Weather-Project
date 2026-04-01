c027f20328edad1f596b3558f0b7059c33740e61
Priyanshu Sahu <priyanshusahu708@gmail.com>
2026-04-01 13:27:31 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -10,23 +10,6 @@ let icon = document.querySelector(".weather-img")
 
 
 
- async function checkWeather(city) {
-    const response = await fetch(URL + city + `&appid=${api_key}`);
-    var data = await response.json()
-    console.log(data)
-     cityname.innerHTML=data.name;
-   temp.innerHTML=Math.round(data.main.temp) + "°C";
-   speed.innerHTML = data.wind.speed + "km/h"
-const api_key = "a36c84991a9fb7f5da92835faccbab19";
-let temp = document.querySelector(".temp")
-let cityname = document.querySelector(".city")
-let humidity = document.querySelector(".humidity")
-let speed = document.querySelector(".speed")
-let search_icon = document.querySelector(".searchbox button")
-let search = document.querySelector(".searchbox input")
-let icon = document.querySelector(".weather-img")
-
-
 
  async function checkWeather(city) {
     const response = await fetch(URL + city + `&appid=${api_key}`);
@@ -68,13 +51,6 @@ search_icon.addEventListener("click",()=>{
 
 
 
-
-
-
-
-
-
-
    humidity.innerHTML = data.main.humidity + "%";
  
    if(data.weather[0].main =="Clouds"){
@@ -96,8 +72,7 @@ search_icon.addEventListener("click",()=>{
    else if(data.weather[0].main =="Rain"){
       icon.src= "assets/rain.png"
    }
- }
-
+ 
 
    // document.querySelector(".weather").style.display = "block"
 
